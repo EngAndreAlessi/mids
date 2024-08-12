78f61fcf9d3bdbcccc49eb70c608720f0b2e4117
André Eduardo Alessi <andreedalessi@gmail.com>
2024-08-12 13:51:46 -0300

    Delete outdated script

--- scripts/results-time-clean.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-time_results <- read_csv("data/results/time-results-raw.csv", show_col_types = FALSE)
-
-time_clean <- time_results |>
-    group_by(instances) |>
-    summarise(mean = mean(times), std = sd(times))
-
-write_csv(time_clean, "data/results/time-results-clean.csv")
