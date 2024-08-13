a584c70a2e024a7f8716e208aab360f0869e789c
Andre Alessi <andreedalessi@gmail.com>
2024-08-13 19:12:23 -0300

    Updated results_analysis and executed it again, generating new log, data and plots

--- scripts/results_analysis.R
+++ scripts/results_analysis.R
@@ -2,79 +2,111 @@ library(tidyverse)
 library(dunn.test)
 library(logr)
 
+# Opening log
+logr::log_open("~/mids/logs/results_analysis.log")
+logr::log_print("STARTING RESULTS ANALYSIS")
+
 # Reading the necessary data
-instances <- read_csv("data/source/instances.csv", show_col_types = FALSE)
-results <- read_csv("data/results/results-raw.csv", show_col_types = FALSE)
-literature <- read_csv("data/source/literature_results.csv", show_col_types = FALSE)
-info <- read_csv("data/results/instances_info.csv", show_col_types = FALSE)
+logr::log_print("Reading the necessary data...")
+instances <- readr::read_csv("data/source/instances.csv", show_col_types = FALSE)
+results <- readr::read_csv("data/results/results-raw.csv", show_col_types = FALSE)
+literature <- readr::read_csv("data/source/literature_results.csv", show_col_types = FALSE)
+info <- readr::read_csv("data/results/instances_info.csv", show_col_types = FALSE)
+logr::log_print("Reading data DONE.")
 
 # Summary
+logr::log_print("Creating summary of results...")
 results_summary <- results |>
-    group_by(instances) |>
-    summarise(avg = mean(solutions),
+    dplyr::group_by(instances) |>
+    dplyr::summarise(avg = mean(solutions),
               std = sd(solutions),
               min = min(solutions))
-
+logr::log_print("Creating summary DONE.")
 # Write to a csv file
-write_csv(results_summary, "data/results/results-summary.csv")
+readr::write_csv(results_summary, "data/results/results-summary.csv")
+logr::log_print("Summary saved to data/results/results-summary.csv")
+
+logr::log_print("Cleaning data...")
 
 # Get only the average to compare with literature results
 greedy_results <- results_summary |>
-    select(instances, avg)
+    dplyr::select(instances, avg)
 
 literature <- literature |>
-    select(instance, dataset, avg, method)
+    dplyr::select(instance, dataset, avg, method)
 
 # Joining with instances data to get dataset column
-greedy_results <- inner_join(greedy_results, instances, by = join_by(instances == name))
+greedy_results <- dplyr::inner_join(greedy_results, instances, by = join_by(instances == name))
 
 # Adding a column "method" with the value "greedy" and renaming column instances
 greedy_results <- greedy_results |>
-    mutate(method = "GREEDY") |>
-    rename(instance = instances)
+    dplyr::mutate(method = "GREEDY") |>
+    dplyr::rename(instance = instances)
 
 # Combining tables greedy_results and literature by binding rows
-all_methods <- bind_rows(literature, greedy_results)
+all_methods <- dplyr::bind_rows(literature, greedy_results)
 
 # Arranging by first column and converting necessary columns to factors
 all_methods <- all_methods |>
-    arrange(instance) |>
-    mutate(dataset = as_factor(dataset),
-           method = as_factor(method))
-
-# Saving this table
-write_csv(all_methods, "data/results/results-clean.csv")
+    dplyr::arrange(instance) |>
+    dplyr::mutate(dataset = as_factor(dataset),
+           method = forcats::as_factor(method))
 
 # Relative deviation from lower bound
 info_lbs <- info |>
-    select(instances, lbs)
+    dplyr::select(instances, lbs)
 
-all_methods <- inner_join(all_methods, info_lbs, by = join_by(instance == instances))
+all_methods <- dplyr::inner_join(all_methods, info_lbs, by = join_by(instance == instances))
 
 all_methods <- all_methods |>
-    mutate(ardp = (avg-lbs)/lbs * 100)
+    dplyr::mutate(ardp = (avg-lbs)/lbs * 100)
+
+logr::log_print("Data cleaning DONE.")
+
+# Saving this table
+readr::write_csv(all_methods, "data/results/results-clean.csv")
+logr::log_print("Cleaned data saved to data/results/results-clean.csv")
+
+# Statistical tests for all data
+logr::log_print("Starting statistical tests...")
 
 # Creating a linear model
 model <- lm(ardp ~ method, data = all_methods)
+logr::log_print(model)
 
 # Getting the residuals
 res <- residuals(model)
 
 # Checking Q-Q plot
 
-qqnorm(res)
-qqline(res, col = "red")
+qqplot <- ggplot2::ggplot(all_methods, aes(sample = ardp)) +
+    stat_qq() +
+    theme_minimal() +
+    labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
+
+ggplot2::ggsave("data/plots/qqplot.png", qqplot, width = 8, height = 6, dpi = 300)
+logr::log_print("QQ plot saved to data/plots/qqplot.png")
 
 # Shapiro-Wilk normality test
 sw_test <- shapiro.test(res)
+logr::log_print(sw_test)
 
 # Very likely not normal, Kruskal-Wallis rank sum test
 kw_test <- kruskal.test(ardp ~ method, data = all_methods)
+logr::log_print(kw_test)
 
 # Wilcoxon Rank-Sum tests for each pair of methods
 w_test <- pairwise.wilcox.test(all_methods$ardp, all_methods$method, p.adjust.method = "bonferroni")
+logr::log_print(w_test)
 
 # Boxplot
-ggplot(all_methods, aes(x = method, y = ardp)) +
+g <- ggplot(all_methods, aes(x = method, y = ardp, color = method)) +
     geom_boxplot() +
-    theme_minimal()
+    theme_minimal() +
+    theme(legend.position = "none") +
+    labs(x = NULL, y = "RDP")
+
+ggplot2::ggsave("data/plots/boxplot.png", g, width = 8, height = 6, dpi = 300)
+logr::log_print("Boxplot saved to data/plots/boxplot.png")
+
+logr::log_print("ANALYSIS DONE")
